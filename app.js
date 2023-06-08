61bdd13e3622765ea113396fccc670f3245524ec
gary909 <garywhite61@hotmail.com>
2023-06-08 13:00:47 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 function checkExam(array1, array2) {
-    // good luck
+    let myScore = 0
+    for(let i = 0; i < array1.length; i++){
+        if(array1[i] == array2[i]){
+            myScore = myScore + 4;
+        } else if (array1[i] != array2[i] && array2[i] == ""){
+            myScore = myScore + 0;
+        } else {
+            myScore = myScore -1 ;
+        }
+    }
+    if(myScore < 0){
+        return 0;
+    } else {
+        return myScore;
+    }
 }
 
 console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"])); // 6
